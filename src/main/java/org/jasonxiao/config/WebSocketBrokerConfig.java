4732f82954a56732afc474e0c615eafdac331ab2
Jason Xiao <jason.xiao@emc.com>
2016-08-28 22:12:52 +0800

    add rabbitmq config

--- src/main/java/org/jasonxiao/config/WebSocketBrokerConfig.java
+++ src/main/java/org/jasonxiao/config/WebSocketBrokerConfig.java
@@ -1,5 +1,7 @@
 package org.jasonxiao.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
@@ -13,6 +15,19 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @EnableWebSocketMessageBroker
 public class WebSocketBrokerConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
+    private final String host;
+    private final String username;
+    private final String password;
+
+    @Autowired
+    public WebSocketBrokerConfig(@Value("${spring.rabbitmq.host}") String host,
+                                 @Value("${spring.rabbitmq.username}") String username,
+                                 @Value("${spring.rabbitmq.password}") String password) {
+        this.host = host;
+        this.username = username;
+        this.password = password;
+    }
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/socket/message").withSockJS();
@@ -21,8 +36,12 @@ public class WebSocketBrokerConfig extends AbstractWebSocketMessageBrokerConfigu
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app")
-                .enableStompBrokerRelay("/topic");
-//                .enableSimpleBroker("/topic");
+                .enableStompBrokerRelay("/topic")
+                .setRelayHost(host)
+                .setClientLogin(username)
+                .setClientPasscode(password)
+                .setSystemLogin(username)
+                .setSystemPasscode(password);
     }
 
 }
