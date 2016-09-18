8a423b6a7facb4142bab7b36ee1cc61452e33370
Jason Xiao <jason.xiao@emc.com>
2016-09-19 00:37:32 +0800

    update test

--- src/test/java/org/jasonxiao/service/GroupServiceImplTest.java
+++ src/test/java/org/jasonxiao/service/GroupServiceImplTest.java
@@ -1,26 +1,27 @@
 package org.jasonxiao.service;
 
-import org.jasonxiao.ApplicationTests;
 import org.jasonxiao.model.Group;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
-
 /**
  * @author Jason Xiao
  */
-//@RunWith(SpringRunner.class)
-public class GroupServiceImplTest extends ApplicationTests{
+@RunWith(SpringRunner.class)
+@DataJpaTest
+//@SpringBootTest
+@ComponentScan
+public class GroupServiceImplTest {
 
     @Autowired
-    private GroupService groupService;
+    GroupService groupService;
 
     @Test
     public void getAll() throws Exception {
